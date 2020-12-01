6b1aac7a26403dfe138f176cb610cceaf4e74e9c
Marek Dudek <marek.dudek@gmail.com>
2020-12-01 22:14:27 +0100

    made executable

--- deploy-java-apps/linux-and-tomcat/java-postgres/jdbc-access/bin/set-up-database.sh
+++ deploy-java-apps/linux-and-tomcat/java-postgres/jdbc-access/bin/set-up-database.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 az group create \
-    --name $AZ_RESOURCE_GROUP \
-    --location $AZ_LOCATION \
+    --name "$AZ_RESOURCE_GROUP" \
+    --location "$AZ_LOCATION" \
     | jq
 
-az postgres server create \
-    --resource-group $AZ_RESOURCE_GROUP \
-    --name $AZ_DATABASE_NAME \
-    --location $AZ_LOCATION \
+az postgres server create --resource-group "$AZ_RESOURCE_GROUP" \
+    --name "$AZ_DATABASE_NAME" \
+    --location "$AZ_LOCATION" \
     --sku-name B_Gen5_1 \
     --storage-size 5120 \
-    --admin-user $AZ_POSTGRESQL_USERNAME \
-    --admin-password $AZ_POSTGRESQL_PASSWORD \
+    --admin-user "$AZ_POSTGRESQL_USERNAME" \
+    --admin-password "$AZ_POSTGRESQL_PASSWORD" \
     | jq
 
 az postgres server firewall-rule create \
-    --resource-group $AZ_RESOURCE_GROUP \
-    --name $AZ_DATABASE_NAME-database-allow-local-ip \
-    --server $AZ_DATABASE_NAME \
-    --start-ip-address $AZ_LOCAL_IP_ADDRESS \
-    --end-ip-address $AZ_LOCAL_IP_ADDRESS \
+    --resource-group "$AZ_RESOURCE_GROUP" \
+    --name "$AZ_DATABASE_NAME"-database-allow-local-ip \
+    --server "$AZ_DATABASE_NAME" \
+    --start-ip-address "$AZ_LOCAL_IP_ADDRESS" \
+    --end-ip-address "$AZ_LOCAL_IP_ADDRESS" \
     | jq
 
 az postgres db create \
-    --resource-group $AZ_RESOURCE_GROUP \
+    --resource-group "$AZ_RESOURCE_GROUP" \
     --name demo \
-    --server-name $AZ_DATABASE_NAME \
+    --server-name "$AZ_DATABASE_NAME" \
     | jq
--- deploy-java-apps/linux-and-tomcat/java-postgres/jdbc-access/bin/tear-down.sh
+++ deploy-java-apps/linux-and-tomcat/java-postgres/jdbc-access/bin/tear-down.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 az group delete \
-    --name $AZ_RESOURCE_GROUP \
+    --name "$AZ_RESOURCE_GROUP" \
     --yes
