e11e4dbafa835c541275390f07dfb56e1d5d674b
Marek Dudek <marek.dudek@gmail.com>
2020-12-05 17:14:30 +0100

    started learning Docker

---
+++ docker/Docker.mm
@@ -0,0 +1,4 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1607163723091" ID="ID_1319806003" MODIFIED="1607163730483" TEXT="Docker"/>
+</map>
