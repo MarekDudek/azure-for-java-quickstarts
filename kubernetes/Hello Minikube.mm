9e0b380135074f1d9a02edb843c8798c2ed728ba
Marek Dudek <marek.dudek@gmail.com>
2020-12-13 22:12:30 +0100

    Kubernetes notes

---
+++ kubernetes/Hello Minikube.mm
@@ -0,0 +1,6 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1607634824022" ID="ID_1401538590" MODIFIED="1607634831673" TEXT="Hello Minikube">
+<node CREATED="1607634872293" ID="ID_1922228100" MODIFIED="1607634877009" POSITION="right" TEXT="minikube start"/>
+</node>
+</map>
