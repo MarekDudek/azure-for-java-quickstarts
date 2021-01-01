85969a8b84505c08873b7acdbf7105c7fb8b8987
Marek Dudek <marek.dudek@gmail.com>
2021-01-01 06:24:37 +0100

    renamed

--- kubernetes/welcome-minikube/generate/generate-dpl-1.sh
+++ kubernetes/welcome-minikube/generate/generate-dpl-1.sh
@@ -12,11 +12,11 @@ MANIFESTS=./../manifests/
 kubectl config set-context ctx-1 --cluster=minikube --user=minikube --namespace=ns-1
 kubectl config use-context ctx-1 
 
-DEPLOYMENT_1=${MANIFESTS}/deployment-2.yaml
+DEPLOYMENT_1=${MANIFESTS}/deployment-1.yaml
 kubectl create deployment dpl-1 --image=k8s.gcr.io/echoserver:1.4 --namespace=ns-1 --dry-run=client -o yaml > ${DEPLOYMENT_1}
 kubectl create -f ${DEPLOYMENT_1}
 
-SERVICE_1=${MANIFESTS}/service-2.yaml
+SERVICE_1=${MANIFESTS}/service-1.yaml
 kubectl expose deployment dpl-1 --type=LoadBalancer --port=8080 --name=service-2   --dry-run=client -o yaml > ${SERVICE_1}
 kubectl create -f ${SERVICE_1}
 
