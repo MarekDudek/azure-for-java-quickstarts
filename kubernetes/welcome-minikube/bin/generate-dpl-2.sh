6723aaff0c16aeb4af4be22ee736fc5194bc9fd4
Marek Dudek <marek.dudek@gmail.com>
2021-01-01 15:49:04 +0100

    generation uses different namespace

--- kubernetes/welcome-minikube/bin/generate-dpl-2.sh
+++ kubernetes/welcome-minikube/bin/generate-dpl-2.sh
@@ -7,13 +7,13 @@ set -euox pipefail
 
 pushd ${THIS_DIR}
 
-kubectl config set-context ctx-1 --cluster=minikube --user=minikube --namespace=ns-1
-kubectl config use-context ctx-1 
+kubectl config set-context pre-test --cluster=minikube --user=minikube --namespace=pre-test
+kubectl config use-context pre-test
 
-kubectl apply  -f ../manifests/ns-1.yaml
+kubectl apply  -f ../manifests/pre-test-namespace.yaml
 
 DEPLOYMENT=../manifests/deployment-2.yaml
-kubectl create deployment dpl-2 --image=k8s.gcr.io/echoserver:1.4 --namespace=ns-1 --dry-run=client -o yaml > ${DEPLOYMENT}
+kubectl create deployment dpl-2 --image=k8s.gcr.io/echoserver:1.4 --dry-run=client -o yaml > ${DEPLOYMENT}
 kubectl create -f ${DEPLOYMENT}
 
 SERVICE=../manifests/service-2.yaml
@@ -22,5 +22,8 @@ kubectl create -f ${SERVICE}
 
 kubectl delete -f ${SERVICE}
 kubectl delete -f ${DEPLOYMENT}
+
+kubectl config use-context minikube
+kubectl config delete-context pre-test
 
 popd
