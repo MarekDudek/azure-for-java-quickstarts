3d20a1124872a789554d3bb461fac02fb317d132
Marek Dudek <marek.dudek@gmail.com>
2021-01-01 03:40:52 +0100

    context operations

--- kubernetes/welcome-minikube/suite.sh
+++ kubernetes/welcome-minikube/suite.sh
@@ -10,7 +10,11 @@ minikube delete --all
 
 minikube start
 
+kubectl config set-context ctx-1 --cluster=minikube --user=minikube --namespace=ns-1
+
 ${THIS_DIR}/set-up-both-dpl-1-2.sh
 ${THIS_DIR}/tear-down-both-dpl-1-2.sh
+
+kubectl config delete-context ctx-1
 
 minikube stop 
