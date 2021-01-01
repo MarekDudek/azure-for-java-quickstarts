50fb446165440467634f5e7b8120511294326def
Marek Dudek <marek.dudek@gmail.com>
2021-01-01 06:25:36 +0100

    renamed variables

--- kubernetes/welcome-minikube/generate/generate-dpl-1.sh
+++ kubernetes/welcome-minikube/generate/generate-dpl-1.sh
@@ -12,15 +12,15 @@ MANIFESTS=./../manifests/
 kubectl config set-context ctx-1 --cluster=minikube --user=minikube --namespace=ns-1
 kubectl config use-context ctx-1 
 
-DEPLOYMENT_1=${MANIFESTS}/deployment-1.yaml
-kubectl create deployment dpl-1 --image=k8s.gcr.io/echoserver:1.4 --namespace=ns-1 --dry-run=client -o yaml > ${DEPLOYMENT_1}
-kubectl create -f ${DEPLOYMENT_1}
+DEPLOYMENT=${MANIFESTS}/deployment-1.yaml
+kubectl create deployment dpl-1 --image=k8s.gcr.io/echoserver:1.4 --namespace=ns-1 --dry-run=client -o yaml > ${DEPLOYMENT}
+kubectl create -f ${DEPLOYMENT}
 
-SERVICE_1=${MANIFESTS}/service-1.yaml
-kubectl expose deployment dpl-1 --type=LoadBalancer --port=8080 --name=service-2   --dry-run=client -o yaml > ${SERVICE_1}
-kubectl create -f ${SERVICE_1}
+SERVICE=${MANIFESTS}/service-1.yaml
+kubectl expose deployment dpl-1 --type=LoadBalancer --port=8080 --name=service-2   --dry-run=client -o yaml > ${SERVICE}
+kubectl create -f ${SERVICE}
 
-kubectl delete -f ${SERVICE_1}
-kubectl delete -f ${DEPLOYMENT_1}
+kubectl delete -f ${SERVICE}
+kubectl delete -f ${DEPLOYMENT}
 
 popd
