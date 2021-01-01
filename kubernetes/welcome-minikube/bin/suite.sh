b4e9efac48e749551f021abb561ad726ae68b327
Marek Dudek <marek.dudek@gmail.com>
2021-01-01 07:11:38 +0100

    entire suite cleaned up

--- kubernetes/welcome-minikube/bin/suite.sh
+++ kubernetes/welcome-minikube/bin/suite.sh
@@ -4,16 +4,20 @@ THIS_DIR=`dirname ${THIS_FILE}`
 IFS=$'\n\t'
 set -euox pipefail
 
+pushd ${THIS_DIR}
+
 minikube delete --all
 
 minikube start
 
 kubectl config set-context ctx-1 --cluster=minikube --user=minikube --namespace=ns-1
 
-${THIS_DIR}/bin/set-up-both-dpl-1-2.sh
-${THIS_DIR}/bin/tear-down-both-dpl-1-2.sh
+./set-up-both-dpl-1-2.sh
+./tear-down-both-dpl-1-2.sh
 
 kubectl config use-context minikube
 kubectl config delete-context ctx-1
 
 minikube stop 
+
+popd
