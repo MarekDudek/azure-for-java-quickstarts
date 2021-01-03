31b165ee4121867d48ca8874edba6cce43dfa76f
Marek Dudek <marek.dudek@gmail.com>
2021-01-03 01:51:54 +0100

    starting with terminal and executing on already running

---
+++ kubernetes/welcome-minikube/bin/run-interactive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+THIS_FILE=$(readlink -f $0)
+THIS_DIR=`dirname ${THIS_FILE}`
+IFS=$'\n\t'
+set -euox pipefail
+
+kubectl run busybox --image=busybox --rm -it -- /bin/sh
+kubectl exec -it nginx -- /bin/sh
