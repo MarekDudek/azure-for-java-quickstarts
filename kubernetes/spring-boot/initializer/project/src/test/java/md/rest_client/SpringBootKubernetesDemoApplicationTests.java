cfe05bd97f20b7a4cf2e330cb1c658531983e7d8
Marek Dudek <marek.dudek@gmail.com>
2021-01-10 20:57:46 +0100

    consuming REST API scheduled

---
+++ kubernetes/spring-boot/initializer/project/src/test/java/md/rest_client/SpringBootKubernetesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package md.rest_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKubernetesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
