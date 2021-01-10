3ed6ba7d1103811f9489821a9de01020447ef018
Marek Dudek <marek.dudek@gmail.com>
2021-01-10 21:52:48 +0100

    images built

---
+++ kubernetes/demo/rest-server/bin/build-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+THIS_FILE=$(readlink -f "$0")
+THIS_DIR=$(dirname "${THIS_FILE}")
+IFS=$'\n\t'
+set -euox pipefail
+
+pushd "${THIS_DIR}"
+
+pushd ..
+
+./mvnw spring-boot:build-image
+
+popd
+
+popd
