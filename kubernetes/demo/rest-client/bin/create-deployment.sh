8f50320b703d45e03865f2a753b8ea7eb2fab95c
Marek Dudek <marek.dudek@gmail.com>
2021-01-11 18:28:11 +0100

    best practices: probles + lifecycle

--- kubernetes/demo/rest-client/bin/create-deployment.sh
+++ kubernetes/demo/rest-client/bin/create-deployment.sh
@@ -10,11 +10,11 @@ pushd ../kubernetes
 DEPLOYMENT=rest-client-deployment.yaml
 kubectl create deployment rest-client --image marekdudek/rest-client --port 8081 -o yaml --dry-run=client > ${DEPLOYMENT}
 patch ${DEPLOYMENT} best-practices.patch
-#kubectl create -f ${DEPLOYMENT}
+kubectl create -f ${DEPLOYMENT}
 
 SERVICE=rest-client-service.yaml
 kubectl expose deployment rest-client --port 7071 --target-port=8081 --type=NodePort -o yaml --dry-run=client > ${SERVICE}
-#kubectl create -f .${SERVICE}
+kubectl create -f .${SERVICE}
 
 popd
 popd
