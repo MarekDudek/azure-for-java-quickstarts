0528e7439455c591d392273b85c8e9bbac58e584
Marek Dudek <marek.dudek@gmail.com>
2021-01-14 23:25:50 +0100

    postgres started on docker

---
+++ kubernetes/demo/postgres/bin/docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+THIS_FILE=$(readlink -f "$0")
+THIS_DIR=$(dirname "${THIS_FILE}")
+IFS=$'\n\t'
+set -euox pipefail
+
+docker run --name my-postgres -e POSTGRES_PASSWORD=marek -d postgres
+docker stop my-postgres
+docker start my-postgres
+sleep 10
+docker exec -it my-postgres psql -U postgres
+docker stop my-postgres
+docker rm my-postgres
+
