e83e5e12821a8e0eae6c90e4fc012b05ae30aab4
Marek Dudek <marek.dudek@gmail.com>
2021-01-17 15:50:46 +0100

    fixed AKS instance creation

--- kubernetes/demo/create-kubernetes-cluster.ps1
+++ kubernetes/demo/create-kubernetes-cluster.ps1
@@ -1,5 +1,1 @@
-az aks create `
-    --resource-group k8s-rg `
-    --name k8s `
-    --node-count 2 `
-    --generate-ssh-keys
+az aks create --resource-group k8s-rg --name k8s --node-count 2 --generate-ssh-keys
