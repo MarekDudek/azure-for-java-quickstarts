91285392d048092d73f6596b208ee5837072524e
Marek Dudek <marek.dudek@gmail.com>
2021-01-17 13:29:00 +0100

    postgres deployment script

---
+++ kubernetes/demo/postgres/bin/create-deployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+THIS_FILE=$(readlink -f "$0")
+THIS_DIR=$(dirname "${THIS_FILE}")
+IFS=$'\n\t'
+set -euox pipefail
+
+pushd "${THIS_DIR}"
+source common.sh
+pushd ../kubernetes
+
+kubectl create -f "${CONFIG}"
+
+popd ; popd
