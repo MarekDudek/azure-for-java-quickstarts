430d5889df8e853e78d146d19621d56be5221298
Marek Dudek <marek.dudek@gmail.com>
2021-01-23 11:55:49 +0100

    deployment generated

--- kubernetes/haskell/haskell-kubernetes-demo/app/Main.hs
+++ kubernetes/haskell/haskell-kubernetes-demo/app/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Lib
 import System.Command
 import qualified Data.Yaml as Y
 
@@ -8,5 +9,4 @@ main :: IO ()
 main = do
   --Stdout out <- cmd "kubectl create deployment rest-server --image marekdudek/rest-server --port 8082 -o yaml --dry-run=client"
   --writeFile "./out/rest-server-deployment.yaml" out
-  res <- Y.decodeThrow "[1, 2, 3]"
-  print (res :: [Integer])
+  decodeDeployment
--- kubernetes/haskell/haskell-kubernetes-demo/src/Lib.hs
+++ kubernetes/haskell/haskell-kubernetes-demo/src/Lib.hs
@@ -1,6 +1,66 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+
 module Lib
-    ( someFunc
+    ( decodeDeployment
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Yaml as Y
+import Data.Yaml.Aeson
+import Text.RawString.QQ
+import Data.ByteString (ByteString)
+import System.IO
+import GHC.Generics
+import Data.Aeson
+import qualified Data.ByteString.Lazy.UTF8 as BLU
+
+configYaml :: ByteString
+configYaml = [r|
+resolver: lts-3.7
+packages:
+  - ./yesod-core
+  - ./yesod-static
+  - ./yesod-persistent
+  - ./yesod-newsfeed
+  - ./yesod-form
+  - ./yesod-auth
+  - ./yesod-auth-oauth
+  - ./yesod-sitemap
+  - ./yesod-test
+  - ./yesod-bin
+  - ./yesod
+  - ./yesod-eventsource
+  - ./yesod-websockets
+
+# Needed for LTS 2
+extra-deps:
+- wai-app-static-3.1.4.1
+|]
+
+
+decodeDeployment :: IO ()
+decodeDeployment = do
+  putStrLn "Decoding Deployment"
+  handle <- openFile "src/deployment.yaml" ReadMode
+  contents <- hGetContents handle
+  print contents
+  let contentsBS :: ByteString = "apiVersion: apps/v1\nkind: Deployment"
+  res <- Y.decodeThrow contentsBS
+  print (res :: Deployment)
+  Y.encodeFile "src/deployment2.yaml" res
+
+
+
+data Deployment =
+  Deployment {
+    apiVersion :: String,
+    kind :: String
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON Deployment --where
+
+instance Y.ToJSON Deployment where
+  toEncoding = genericToEncoding defaultOptions
