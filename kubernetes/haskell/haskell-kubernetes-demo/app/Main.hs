63bdc42f5f36f67496dc93df2220c152d1d582a9
Marek Dudek <marek.dudek@gmail.com>
2021-01-23 03:04:10 +0100

    yaml test

--- kubernetes/haskell/haskell-kubernetes-demo/app/Main.hs
+++ kubernetes/haskell/haskell-kubernetes-demo/app/Main.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.Command
+import qualified Data.Yaml as Y
 
 main :: IO ()
 main = do
-  Stdout out <- cmd "kubectl create deployment rest-server --image marekdudek/rest-server --port 8082 -o yaml --dry-run=client"
-  writeFile "./out/rest-server-deployment.yaml" out
+  --Stdout out <- cmd "kubectl create deployment rest-server --image marekdudek/rest-server --port 8082 -o yaml --dry-run=client"
+  --writeFile "./out/rest-server-deployment.yaml" out
+  res <- Y.decodeThrow "[1, 2, 3]"
+  print (res :: [Integer])
