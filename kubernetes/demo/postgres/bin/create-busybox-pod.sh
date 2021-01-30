3d582c907643487d6bef5390243674852d5d2313
Marek Dudek <marek.dudek@gmail.com>
2021-01-30 16:21:24 +0100

    busybox with volume mount

--- kubernetes/demo/postgres/bin/create-busybox-pod.sh
+++ kubernetes/demo/postgres/bin/create-busybox-pod.sh
@@ -8,6 +8,6 @@ pushd "${THIS_DIR}"
 source common.sh
 pushd ../kubernetes
 
-kubectl run busybox --image=busybox --port=8080 --dry-run=client -o yaml > "${BUSYBOX}" -- /bin/sh -c "while true; do sleep 5; done"
+kubectl run busybox --image=busybox --port=8080 --dry-run=client -o yaml > "${BUSYBOX}" -- /bin/sh -c "while true; do sleep 1; done"
 
 popd
