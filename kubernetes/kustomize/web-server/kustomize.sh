a6ac7bdb60f45c7e38631d59dee9c1bfd07cd2d9
Marek Dudek <marek.dudek@gmail.com>
2021-01-31 15:09:17 +0100

    web-server kustomizations split

--- kubernetes/kustomize/web-server/kustomize.sh
+++ kubernetes/kustomize/web-server/kustomize.sh
@@ -5,6 +5,21 @@ DIR=$(realpath --relative-to=. "${DIR}")
 IFS=$'\n\t'
 set -euxo pipefail
 
+rm -fr "${DIR}"/build/*
+mkdir "${DIR}"/build/production
+mkdir "${DIR}"/build/development
 
-kubectl kustomize "${DIR}"/app/overlays/production  > "${DIR}"/build/production.yaml
-kubectl kustomize "${DIR}"/app/overlays/development > "${DIR}"/build/development.yaml
+kubectl kustomize "${DIR}"/app/overlays/production  > "${DIR}"/build/production/production.yaml
+kubectl kustomize "${DIR}"/app/overlays/development > "${DIR}"/build/development/development.yaml
+
+pushd "${DIR}"/build/production
+csplit --prefix=manifest- --suffix="%d.yaml" --suppress-matched --elide-empty-files production.yaml '/---/' '{*}'
+mv manifest-0.yaml service.yaml
+mv manifest-1.yaml deployment.yaml
+popd
+
+pushd "${DIR}"/build/development
+csplit --prefix=manifest- --suffix="%d.yaml" --suppress-matched --elide-empty-files development.yaml '/---/' '{*}'
+mv manifest-0.yaml service.yaml
+mv manifest-1.yaml deployment.yaml
+popd
