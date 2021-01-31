3cca5650537f560e18725e61cb18158778af1753
Marek Dudek <marek.dudek@gmail.com>
2021-01-31 02:15:25 +0100

    longer command

--- kubernetes/kustomize/web-client/create-dry-run.sh
+++ kubernetes/kustomize/web-client/create-dry-run.sh
@@ -7,4 +7,11 @@ set -euxo pipefail
 
 
 NAME=web-client
-kubectl create deployment ${NAME} --image=alpine -o yaml --dry-run=client > "${DIR}"/k8s/deployment.yaml -- /bin/sh -c "while true; do wget web-server:7072 -O-; sleep 1; done"
+kubectl create deployment ${NAME} --image=alpine -o yaml --dry-run=client > "${DIR}"/k8s/deployment.yaml -- /bin/sh -c "
+echo Querying web server
+while true;
+do
+  wget web-server:7072 -O-
+  sleep 1
+done
+"
