587131101a7c8a35b78ab57b4411cbb5963f5380
Marek Dudek <marek.dudek@gmail.com>
2021-01-31 01:38:04 +0100

    web-client connects to web-server

--- kubernetes/kustomize/web-client/create-dry-run.sh
+++ kubernetes/kustomize/web-client/create-dry-run.sh
@@ -7,4 +7,4 @@ set -euxo pipefail
 
 
 NAME=web-client
-kubectl create deployment ${NAME} --image=alpine -o yaml --dry-run=client > "${DIR}"/k8s/deployment.yaml -- sleep 300
+kubectl create deployment ${NAME} --image=alpine -o yaml --dry-run=client > "${DIR}"/k8s/deployment.yaml -- /bin/sh -c "while true; do wget web-server:7072 -O-; sleep 1; done"
