59c958db49c542120613d59e941dc4aec96aba44
Marek Dudek <marek.dudek@gmail.com>
2021-02-02 00:00:41 +0100

    scripts cleaned-up

--- kubernetes/kustomize/splitting.sh
+++ kubernetes/kustomize/splitting.sh
@@ -2,10 +2,11 @@
 set -euxo pipefail
 
 function split() {
+    set +x
     BUILD="$1"
     FILE="$2"
     pushd "$BUILD"
-    csplit --prefix=manifest- --suffix="%d.yaml" --suppress-matched --elide-empty-files "$FILE" '/---/' '{*}'
+    csplit --prefix=manifest- --suffix="%d.yaml" --suppress-matched --elide-empty-files --quiet "$FILE" '/---/' '{*}'
     NAMES=("${@:3}")
     COUNTER=-1
     for NAME in "${NAMES[@]}"
@@ -14,4 +15,5 @@ function split() {
       mv manifest-"$COUNTER".yaml "$NAME"
     done
     popd
+    set -x
 }
--- kubernetes/kustomize/web-client/kustomize.sh
+++ kubernetes/kustomize/web-client/kustomize.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 SCRIPT=$(readlink -f "$0")
-DIR=$(dirname "${SCRIPT}")
-DIR=$(realpath --relative-to=. "${DIR}")
+DIR=$(dirname "$SCRIPT")
+DIR=$(realpath --relative-to=. "$DIR")
 IFS=$'\n\t'
 set -euxo pipefail
-source "${DIR}"/../splitting.sh
+source "$DIR"/../splitting.sh
 
 
-rm -fr "${DIR}"/build/
+rm -fr "$DIR"/build/
 
-mkdir -p "${DIR}"/build/prod
+mkdir -p "$DIR"/build/prod
 kustomize build \
   --load_restrictor=none \
   --reorder=none \
-  --output "${DIR}"/build/prod/__kustomized.yaml \
-  "${DIR}"/app/overlays/prod
-split "${DIR}"/build/prod __kustomized.yaml \
+  --output "$DIR"/build/prod/__kustomized.yaml \
+  "$DIR"/app/overlays/prod
+split "$DIR"/build/prod __kustomized.yaml \
   deployment.yaml
 
-mkdir -p "${DIR}"/build/dev
+mkdir -p "$DIR"/build/dev
 kustomize build \
   --load_restrictor=none \
   --reorder=none \
-  --output "${DIR}"/build/dev/__kustomized.yaml \
-  "${DIR}"/app/overlays/dev
-split "${DIR}"/build/dev __kustomized.yaml \
+  --output "$DIR"/build/dev/__kustomized.yaml \
+  "$DIR"/app/overlays/dev
+split "$DIR"/build/dev __kustomized.yaml \
   deployment.yaml
 
-VERSION="002"
-SNAPSHOT="${DIR}"/versions/v-${VERSION}
-rm -rf "${SNAPSHOT}" && mkdir -p "${SNAPSHOT}"
-cp -r "${DIR}"/build/* "${SNAPSHOT}"
+SNAPSHOT="$DIR"/versions/v-002
+rm -rf "$SNAPSHOT" ; mkdir -p "$SNAPSHOT"
+cp -r "$DIR"/build/* "$SNAPSHOT"
