41bddf98bbb1e431f1674f81aa3ac52acf910adb
Marek Dudek <marek.dudek@gmail.com>
2021-02-01 18:30:42 +0100

    kustomize all script

---
+++ kubernetes/kustomize/kustomize-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "${SCRIPT}")
+DIR=$(realpath --relative-to=. "${DIR}")
+IFS=$'\n\t'
+set -euxo pipefail
+
+"${DIR}"/web-server/create-dry-run.sh
+"${DIR}"/web-server/kustomize.sh
+
+"${DIR}"/web-client/create-dry-run.sh
+"${DIR}"/web-client/kustomize.sh
