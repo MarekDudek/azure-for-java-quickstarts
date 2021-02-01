da62a92a3b48d3ad1e1a8f99730f1cbf5896daf3
Marek Dudek <marek.dudek@gmail.com>
2021-02-01 21:54:33 +0100

    node-port specified

---
+++ kubernetes/kustomize/create-namespaces.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "${SCRIPT}")
+DIR=$(realpath --relative-to=. "${DIR}")
+IFS=$'\n\t'
+set -euxo pipefail
+
+kubectl create namespace prod
+kubectl create namespace dev
--- kubernetes/kustomize/web-server/create-dry-run.sh
+++ kubernetes/kustomize/web-server/create-dry-run.sh
@@ -7,6 +7,17 @@ set -euxo pipefail
 
 
 NAME=web-server
-kubectl create deployment ${NAME} --image=nginx:stable --port=80 -o yaml --dry-run=client > "${DIR}"/app/base/deployment.yaml
-kubectl create service nodeport ${NAME} --tcp=7072:80 -o yaml --dry-run=client > "${DIR}"/app/base/nodeport-service.yaml
-kubectl create service loadbalancer ${NAME}-loadbalancer --tcp=7072:80  -o yaml --dry-run=client > "${DIR}"/app/base/loadbalancer-service.yaml
+
+kubectl create deployment ${NAME} \
+  --image=nginx:stable \
+  --port=80 \
+  -o yaml --dry-run=client > "${DIR}"/app/base/deployment.yaml
+
+kubectl create service nodeport ${NAME} \
+  --node-port=30000 \
+  --tcp=7072:80 \
+  -o yaml --dry-run=client > "${DIR}"/app/base/nodeport-service.yaml
+
+kubectl create service loadbalancer ${NAME}-loadbalancer \
+  --tcp=7072:80  \
+  -o yaml --dry-run=client > "${DIR}"/app/base/loadbalancer-service.yaml
