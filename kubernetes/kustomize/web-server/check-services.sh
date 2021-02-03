c3755c30842f1bc8a5a5dcdeb60022955d36148a
Marek Dudek <marek.dudek@gmail.com>
2021-02-04 00:23:59 +0100

    service check script

--- kubernetes/kustomize/web-server/check-services.sh
+++ kubernetes/kustomize/web-server/check-services.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "${SCRIPT}")
 DIR=$(realpath --relative-to=. "${DIR}")
 IFS=$'\n\t'
 set -euxo pipefail
+
 
 function check_service() {
 
@@ -20,6 +21,31 @@ function check_service() {
   kill $PID
 }
 
+function check_service_minikube() {
+
+    SERVICE_NAME=$1
+
+    URL=$(minikube service "$SERVICE_NAME" --url )
+    wget --spider --quiet "$URL"
+}
+
+function check_service_minikube_alt() {
+
+    SERVICE_NAME=$1
+
+    MINIKUBE_VM_IP=$(minikube ip)
+    PORT=$(kubectl get service "$SERVICE_NAME" --output='jsonpath={.spec.ports[*].nodePort}')
+
+    URL="$MINIKUBE_VM_IP":"$PORT"
+    wget --spider --quiet "$URL"
+}
+
 check_service web-server-cip 7071
 check_service web-server-np  7072
 check_service web-server-lb  7073
+
+check_service_minikube web-server-np
+check_service_minikube web-server-lb
+
+check_service_minikube_alt web-server-np
+check_service_minikube_alt web-server-lb
