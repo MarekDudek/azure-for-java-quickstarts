b6d76e3badffd51210e451b7c5596d0c162f4eb6
Marek Dudek <marek.dudek@gmail.com>
2021-02-07 17:40:41 +0100

    base config for postgres

---
+++ kubernetes/kustomize/three-tier-app/app/create-dry-run/config-map-db.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "${SCRIPT}")
+DIR=$(realpath --relative-to=. "$DIR")
+IFS=$'\n\t'
+set -euxo pipefail
+
+BASE="$DIR"/../base
+
+kubectl create configmap config-map-db \
+  --from-literal POSTGRES_DB=default-db \
+  --from-literal POSTGRES_USER=db-admin \
+  --from-literal POSTGRES_PASSWORD="#xkFB^G],WL2UaU6" \
+  -o yaml --dry-run=client > "$BASE"/config-map-db.yaml
