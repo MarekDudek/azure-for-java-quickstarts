a6b38b3c2cd2049a3f72cb49008ee97395550ac0
Marek Dudek <marek.dudek@gmail.com>
2021-02-07 22:22:58 +0100

    db creation and access

--- kubernetes/kustomize/three-tier-app/app/db/drop-database.sh
+++ kubernetes/kustomize/three-tier-app/app/db/drop-database.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-THIS_FILE=$(readlink -f "$0")
-THIS_DIR=$(dirname "${THIS_FILE}")
-IFS=$'\n\t'
 set -euox pipefail
-
 
 POD=$(kubectl get pod --selector=app=postgres-on-k8s -o jsonpath='{.items[*].metadata.name}')
 
