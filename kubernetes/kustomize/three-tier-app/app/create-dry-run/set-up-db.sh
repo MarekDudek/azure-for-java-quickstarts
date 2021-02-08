8807211cfa8ef417e8acf35aa8fb9a654d8fc252
Marek Dudek <marek.dudek@gmail.com>
2021-02-08 21:14:40 +0100

    jobs parametrized

--- kubernetes/kustomize/three-tier-app/app/create-dry-run/set-up-db.sh
+++ kubernetes/kustomize/three-tier-app/app/create-dry-run/set-up-db.sh
@@ -1,3 +1,3 @@
-PGPASSWORD=postgres createuser --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" db_user
-PGPASSWORD=postgres psql              -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" -c "ALTER USER \"db_user\" WITH PASSWORD 'db_user'"
-PGPASSWORD=postgres createdb   --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" --owner=db_user my_db
+PGPASSWORD="$PASSWORD" createuser --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" db_user
+PGPASSWORD="$PASSWORD" psql              -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" -c "ALTER USER \"db_user\" WITH PASSWORD 'db_user'"
+PGPASSWORD="$PASSWORD" createdb   --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" --owner=db_user my_db
--- kubernetes/kustomize/three-tier-app/app/create-dry-run/tear-down-db.sh
+++ kubernetes/kustomize/three-tier-app/app/create-dry-run/tear-down-db.sh
@@ -1,2 +1,2 @@
-PGPASSWORD=postgres dropdb   --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" my_db
-PGPASSWORD=postgres dropuser --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" db_user
+PGPASSWORD="$PASSWORD" dropdb   --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" my_db
+PGPASSWORD="$PASSWORD" dropuser --echo -U postgres -h "${DB_SERVICE_URL}" -p "${DB_SERVICE_PORT}" db_user
