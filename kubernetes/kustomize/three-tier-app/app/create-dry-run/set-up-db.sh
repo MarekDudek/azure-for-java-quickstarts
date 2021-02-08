088f1427fb152b526fd14bc55480497ca1cb7c7e
Marek Dudek <marek.dudek@gmail.com>
2021-02-08 18:35:36 +0100

    job with postgres image

--- kubernetes/kustomize/three-tier-app/app/create-dry-run/set-up-db.sh
+++ kubernetes/kustomize/three-tier-app/app/create-dry-run/set-up-db.sh
@@ -1,3 +1,3 @@
-
-
-echo I worked
+PGPASSWORD=postgres createuser --echo -U postgres -h postgres-on-k8s-dd db_user
+PGPASSWORD=postgres psql              -U postgres -h postgres-on-k8s-dd -c "ALTER USER \"db_user\" WITH PASSWORD 'db_user'"
+PGPASSWORD=postgres createdb   --echo -U postgres -h postgres-on-k8s-dd --owner=db_user my_db
